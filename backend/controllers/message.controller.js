15677b3b21c041535497a0cb11c0f35239e88b3d
Ashishkumar7005 <ashishsharma24012001@gmail.com>
2024-04-03 17:36:03 +0530

    socket done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async(req,res)=>{
 
@@ -28,13 +29,16 @@ export const sendMessage = async(req,res)=>{
             conversation.message.push(newMessage._id);
         }
 
-        //socket io here
-
+        
+        await Promise.all([conversation.save(), newMessage.save()]);
         // await conversation.save();
         // await newMessage.save();
-
-        await Promise.all([conversation.save(), newMessage.save()]);
-
+        
+        //socket io here
+        const receiverSocketId = getReceiverSocketId(receiverId); 
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage);
+        }
         res.status(201).json(newMessage);
 
     } catch (error) {
